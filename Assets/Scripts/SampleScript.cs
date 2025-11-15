f7a6f5be85992ae4860db07f1a9a7cd5faa708e0
sampleAccountTHS <800024941@fjuhsd.org>
2025-11-15 15:29:52 -0800

    Demonstration change
    Made some change for demonstration purpose

--- Assets/Scripts/SampleScript.cs
+++ Assets/Scripts/SampleScript.cs
@@ -5,7 +5,7 @@ public class SampleScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Oh I started the game");
     }
 
     // Update is called once per frame
